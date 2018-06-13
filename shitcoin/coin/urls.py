fb18bb1bbec134f23e04acf40dc6096628d01af4
rishikant42 <rksbtp@gmail.com>
2018-06-13 16:30:47 +0530

    txn to poo

--- shitcoin/coin/urls.py
+++ shitcoin/coin/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
      url(r'^wallet/new/$', views.NewWallet.as_view()),
      url(r'^make/transaction/$', views.MakeTransaction.as_view()),
      url(r'^generate/transaction/$', views.generate_txn),
+
+
+     url(r'^transactions/new/$', views.txn_new),
+     url(r'^transactions/get/$', views.txn_get),
+     url(r'^nodeindex/$', views.nodeindex),
+     url(r'^mine/$', views.mine),
 ]
